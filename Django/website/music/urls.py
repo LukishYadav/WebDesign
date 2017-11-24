a9eeeac688b84353f0f54c37e18474142e0d5047
Lukish Yadav <yadavlukish@gmail.com>
2017-11-24 12:35:42 +0530

    CDU

--- Django/website/music/urls.py
+++ Django/website/music/urls.py
@@ -7,10 +7,16 @@ urlpatterns = [
     #/music/
     url(r'^$', views.IndexView.as_view(), name='index'),
     #/music/71/
-    url(r'^(?P<pk>[0-9]+)/ $',views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(), name='detail'),
 
     #/music/album/add
-    url(r'album/add/$',views.AlbumCreate.as_view(),name='album-add'), 
+    url(r'^album/add/$',views.AlbumCreate.as_view(),name='album-add'), 
+
+    #/music/album/2
+    url(r'^album/(?P<pk>[0-9]+)/$',views.AlbumUpdate.as_view(),name='album-update'),
+
+
+    url(r'^album/(?P<pk>[0-9]+)/delete/$',views.AlbumDelete.as_view(),name='album-delete'),
     #/music/71/
     #url(r'^(?P<album_id>[0-9]+)/favorite $',views.favorite, name='favorite'),
 ]
